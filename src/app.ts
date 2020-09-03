ef36dcf61c35b86247b90c999a73b753d7ea7c35
thebongy <rishit.bansal0@gmail.com>
2020-09-04 00:05:03 +0530

    feat: Base backend

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+import appRoutes from './routes';
+
+dotenv.config();
+
+const app = express();
+
+app.set("PORT", process.env.PORT || 3000);
+app.use(bodyParser.json());
+
+app.use(appRoutes);
+
+app.listen(app.get("PORT"), () => {
+    console.log("Server listening on port ", app.get("PORT"));
+});
