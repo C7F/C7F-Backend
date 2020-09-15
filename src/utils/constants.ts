f807895c5c8d7e88df61204b43d061591c49e299
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-15 20:36:53 +0530

    refactor: Constants moved to utilities

---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+export const errors = {
+    teamNotUnique: 'teamNotUnique',
+    validationError: 'validationError',
+    invalidCredentials: 'invalidCredentials',
+};
+
+export const database = {
+    connectionSuccess: 'Database connection successful.',
+    connectionFailure: 'Database connection failure.',
+};
