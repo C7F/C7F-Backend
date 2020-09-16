896c4428db1ff0676b01cdcf30951a0dcf516379
thebongy <rishit.bansal0@gmail.com>
2020-09-17 02:36:10 +0530

    feat: Add npm scripts to migrate latest/rollback

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv';
 import { join } from 'path';
 
-dotenv.config();
-
+dotenv.config({
+    path: join(__dirname, '../.env'),
+});
 const config = {
     client: 'postgres',
     connection: process.env.DATABASE_URL,
