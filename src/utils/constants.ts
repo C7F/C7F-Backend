40d3b2b80bc919d9fdaa47a3adf26179ba70c0c0
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-17 01:17:24 +0530

    feat: Added constants for migrations

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -10,3 +10,8 @@ export const database = {
     connectionSuccess: 'Database connection successful.',
     connectionFailure: 'Database connection failure.',
 };
+
+export const migrations = {
+    tableDropped: 'Table dropped: ',
+    tableCreated: 'Table created: ',
+};
