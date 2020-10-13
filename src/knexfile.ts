687156e7edc89c3080407e06f95cf8037a796d19
thebongy <rishit.bansal0@gmail.com>
2020-10-13 13:41:40 +0530

    revert: debugging changes from previous commit

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,10 +1,6 @@
 import dotenv from 'dotenv';
 import { join } from 'path';
-import pg from 'pg';
 
-pg.defaults.ssl = false;
-
-console.log(process.env.DATABASE_URL);
 dotenv.config({
     path: join(__dirname, '../.env'),
 });
