d487d94ea49eec28e2b7829f3bef92a40f402f29
thebongy <rishit.bansal0@gmail.com>
2020-10-13 13:35:08 +0530

    fix: adding some debugging to see why heroku won't connect to db

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,6 +1,10 @@
 import dotenv from 'dotenv';
 import { join } from 'path';
+import pg from 'pg';
 
+pg.defaults.ssl = false;
+
+console.log(process.env.DATABASE_URL);
 dotenv.config({
     path: join(__dirname, '../.env'),
 });
